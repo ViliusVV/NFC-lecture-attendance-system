1065b1c4888ba4fd6d33d971ff3bebfb9cb7d38f
Dovydas <bumblysdovydas@gmail.com>
2019-05-18 13:07:01 +0300

    Added xUnit tests project

---
+++ UnitTests/TestClasses/Class1.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+
+namespace UnitTests
+{
+    public class Class1
+    {
+        // Fact test example
+        // Facts are tests which are always true. They test invariant conditions
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(4, Add(2, 2)); // should be 5 instead of 4 if u want it to fail
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        // Theory test example
+        // Theories are tests which are only true for a particular set of data
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(1)]  // fails if 2,4,6,...
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+
+        bool IsntOdd(int value)
+        {
+            return value % 2 != 1;
+        }
+    }
+}
